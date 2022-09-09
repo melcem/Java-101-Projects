1d7cc6a2dfd13b3d0260bd12ff64e46cab8cae03
melcem <melcem@gmail.com>
2022-09-09 10:32:01 +0300

    Add files via upload

--- Donguler/FaktoriyelHesaplayanProgram/README.md
+++ Donguler/FaktoriyelHesaplayanProgram/README.md
@@ -1,1 +1,10 @@
+# FaktoriyelHesaplayanProgram
+  * Java ile faktöriyel hesaplayan program yazıyoruz.
+
+## Veriler
+  * N elemanlı bir kümenin elemanları ile oluşturulacak r elemanlı farklı grupların sayısı n’in r’li kombinasyonu olarak adlandırılır. N’in r’li kombinasyonu C(n,r) şeklinde gösterilir.
+  * Kombinasyon formülü: C(n,r) = n! / (r! * (n-r)!)
+
+## Ödev
+  * Java ile kombinasyon hesaplayan program yazınız.
 
---
+++ Donguler/FaktoriyelHesaplayanProgram/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* int n,fact=1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Sayi giriniz: ");
+        n = scanner.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            fact*=i;
+        }
+        System.out.println(n + " faktoriyel: " + fact); */
+
+        int c,r;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("C");
+        c = scanner.nextInt();
+
+    }
+}
