d5ea67b87ddf08ce1d0a152d77a0ddfde0a1edf4
melcem <melcem@gmail.com>
2022-09-09 12:01:19 +0300

    Kombinasyon Hesabi tamamlandi.

--- Donguler/FaktoriyelHesaplayanProgram/src/Main.java
+++ Donguler/FaktoriyelHesaplayanProgram/src/Main.java
@@ -14,11 +14,33 @@ public class Main {
         }
         System.out.println(n + " faktoriyel: " + fact); */
 
-        int c,r;
+        int C,n,r;
+        int nFact=1;
+        int rFact=1;
+        int nrFact=1;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("C");
-        c = scanner.nextInt();
+        System.out.print("n sayisini girin: ");
+        n = scanner.nextInt();
 
+        System.out.print("r sayisini girin: ");
+        r = scanner.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            nFact *= i;
+        }
+        System.out.println("n! = " + nFact);
+        for(int i=1; i<=r; i++) {
+            rFact *= i;
+        }
+        System.out.println("r! = " + rFact);
+        for(int i=1; i<=n-r; i++) {
+            nrFact *= i;
+        }
+        System.out.println("(n-r)! = " + nrFact);
+
+        C = nFact/(rFact * nrFact);
+        System.out.println("C(n,r)= " + C);
     }
 }
